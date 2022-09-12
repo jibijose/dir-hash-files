f5ad108f4b37af8d684f427668a7c49e44776694
Jibi Jose <jibijose@yahoo.com>
2022-09-12 21:55:14 +0530

    three way wip

--- trial.bat
+++ trial.bat
@@ -1,15 +1,15 @@
 call mvn clean package
 
-
-
+/**************  simple MD5 checks **********************/
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m createhash -h MD5 -i "C:\pubgroupe" -o fileinfo.xlsx
-
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m comparehash -h MD5 -l "fileinfo.xlsx" -r "C:\PB" -o hashstatus.xlsx
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m comparehash -h MD5 -l "C:\pubgroupe" -r "C:\PB" -o hashstatus.xlsx
 
-
-
+/**************  no hash checks **********************/
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m createhash -i "C:\pubgroupe" -o fileinfo.xlsx
-
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m comparehash -l "fileinfo.xlsx" -r "C:\PB" -o hashstatus.xlsx
 call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m comparehash -l "C:\pubgroupe" -r "C:\PB" -o hashstatus.xlsx
+
+/**************  three way comparison checks **********************/
+call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m createhash -h MD5 -i "C:\pubgroupe" -o fileinfo.xlsx
+call java -jar target\dirhashfiles-1.0-SNAPSHOT-shaded.jar -m comparehash -h MD5 -l "fileinfo.xlsx" -c "C:\pubgroupe" -r "C:\PB" -o hashstatus.xlsx
